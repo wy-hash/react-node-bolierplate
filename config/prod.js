1f4e19ec6eccd1b0ac63818c666e0a9d05ebada4
wy-hash <50538640+wy-hash@users.noreply.github.com>
2022-10-27 17:55:55 +0900

    9강 비밀정보보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI // HEROKU사용시 "MONGO_URI" 똑같이 해줘야 함
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const bodyParser = require("body-parser")
 const { User } = require("./models/User")
+const config = require("./config/key")
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true })); // 클라이언트에서 오는 정보를 서버에서 분석하여 
@@ -10,13 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // 클라이언트에서 오
 app.use(bodyParser.json());
 
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://root:root@boilerplate.otcyafq.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect(config.mongoURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send("Hello World!~~ "))
+app.get('/', (req, res) => res.send("Hello World!~~@@ "))
 
 app.post('/register', (req, res) => {
     // 회원 가입할 때 필요한 정보들을 client에서 가져오면
