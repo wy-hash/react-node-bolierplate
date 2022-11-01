c96c23d768af9daf6db55c5c5c0598988695d799
wy-hash <50538640+wy-hash@users.noreply.github.com>
2022-11-01 18:28:06 +0900

    13강 Auth 기능만들기

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { User } = require('./models/User')
+const { auth } = require('./middleware/auth')
 const config = require("./config/key")
 
 // application/x-www-form-urlencoded
@@ -21,7 +22,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send("Hello World!~~@@ "))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원 가입할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
     console.log(req.body)
@@ -36,7 +37,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 데이터 베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -65,8 +66,20 @@ app.post('/login', (req, res) => {
         })
 
     })
+})
 
-
+app.get('/api/users', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 이야기는 Authentication이 True라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
 })
 
 
